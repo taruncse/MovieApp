db6a0e149afe88ac83b4dcf4264eeb47a1796462
Tarun <tarun.pstu@gmail.com>
2019-06-08 15:34:10 +0600

    * Added News model

---
+++ app/src/main/java/com/tkb/movieapp/models/News.java
@@ -0,0 +1,58 @@
+
+package com.tkb.movieapp.models;
+
+
+public class News {
+
+    private String mTitle;
+
+    private String mSection;
+
+    private String mAuthor;
+
+    private String mDate;
+
+    private String mUrl;
+
+    private String mThumbnail;
+
+    private String mTrailTextHtml;
+
+    public News(String title, String section, String author, String date, String url, String thumbnail, String trailText) {
+        mTitle = title;
+        mSection = section;
+        mAuthor = author;
+        mDate = date;
+        mUrl = url;
+        mThumbnail = thumbnail;
+        mTrailTextHtml = trailText;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public String getSection() {
+        return mSection;
+    }
+
+    public String getAuthor() {
+        return mAuthor;
+    }
+
+    public String getDate() {
+        return mDate;
+    }
+
+    public String getUrl() {
+        return mUrl;
+    }
+
+    public String getThumbnail() {
+        return mThumbnail;
+    }
+
+    public String getTrailTextHtml() {
+        return mTrailTextHtml;
+    }
+}
